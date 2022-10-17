003be12ea16f36ee9a7921e605e679316b5ab166
JennyShen10792 <js10792@nyu.edu>
2022-10-17 14:20:25 -0400

    Edit delete feature

--- delete.py
+++ delete.py
@@ -1,12 +1,11 @@
 from flask import render_template, request, url_for, redirect, flash flask import Flask
 
 
-@app.route('/movie/delete/<int:item_id>', methods=['POST'])
+@app.route('/items/delete/<int:item_id>', methods=['POST'])
 def delete(item_id):
     item = Item.query.get_or_404(item_id)
-    db.session.delete(item)
+    db.session.delete_one(item)
     db.session.commit()
-    flash('Item deleted.')
     return redirect(url_for('index'))
     
     
