12f5ef707cb4b4b9fe53009220c475b3b0eec267
Hillary <hillarydavis@10-17-172-17.dynapool.wireless.nyu.edu>
2022-10-19 15:07:44 -0400

    edit search html

--- app.py
+++ app.py
@@ -44,8 +44,6 @@ def home():
     """
 @app.route('/search', methods=['GET'])
 def search_inventory():
-    #not fully done - I put some variables in place of what they might actually be
-    #not sure about what you all might have named things
     name = request.args.get('name')
     docs = db.exampleapp.find({"name": {"$regex": name, "$options": "i"}})
     if not docs:
